f88ad2f554b441be68ab06e07e05906f79b4eafc
Sebastian Aguirre Duque <sadw621@gmail.com>
2023-05-19 15:00:39 -0500

    Manejo de errores en la página.

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -11,6 +11,9 @@ E-mail: sadw621@gmail.com -->
 const { id } = useRoute().params;
 const url = 'https://fakestoreapi.com/products/' + id;
 const { data: product } = await useFetch(url, { key: id });
+if(!product.value) {
+  throw createError({statusCode: 404, statusMessage: 'Product not found', fatal: true});
+}
 definePageMeta({
   layout: 'products'
 })
